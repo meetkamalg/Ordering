853f4088759462fe33d4b3275d4b73fdac19d56b
Meet Kamal Grewal <142540962+meetkamalg@users.noreply.github.com>
2025-07-17 07:06:05 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+export default function Header() {
+  return (
+    <nav>
+      <h2>🍔 Food Express</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+      </ul>
+    </nav>
+  );
+}
