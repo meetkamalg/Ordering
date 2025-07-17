beee0afa8812b23feec1dd197d5ac9e2f1d2af6b
Meet Kamal Grewal <142540962+meetkamalg@users.noreply.github.com>
2025-07-17 07:07:14 -0600

    Update CartItem.js

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,1 +1,13 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 
+export default function CartItem({ item }) {
+  const { removeFromCart } = useContext(CartContext);
+
+  return (
+    <div className="cart-item">
+      <span>{item.name} - ${item.price}</span>
+      <button onClick={() => removeFromCart(item.id)}>Remove</button>
+    </div>
+  );
+}
