7906b902c1de91425ee5a110e20c4f4b31c408cf
Meet Kamal Grewal <142540962+meetkamalg@users.noreply.github.com>
2025-07-17 07:06:54 -0600

    Update FoodItem.js

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,1 +1,14 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 
+export default function FoodItem({ item }) {
+  const { addToCart } = useContext(CartContext);
+
+  return (
+    <div className="food-item">
+      <h4>{item.name}</h4>
+      <p>${item.price}</p>
+      <button onClick={() => addToCart(item)}>Add to Cart</button>
+    </div>
+  );
+}
