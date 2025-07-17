2b9f6f9ed9540f1487a1eb89228e90ec6de978a1
Meet Kamal Grewal <142540962+meetkamalg@users.noreply.github.com>
2025-07-17 07:07:54 -0600

    Update Menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import FoodItem from '../components/FoodItem';
 
+const sampleMenu = [
+  { id: 1, name: 'Burger', price: 8.99 },
+  { id: 2, name: 'Fries', price: 3.99 },
+  { id: 3, name: 'Pizza Slice', price: 4.99 },
+];
+
+export default function Menu() {
+  return (
+    <div>
+      <h2>Menu</h2>
+      {sampleMenu.map(item => (
+        <FoodItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
