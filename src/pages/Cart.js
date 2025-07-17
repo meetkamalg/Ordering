9d2e04b9f0007037ba4bd53762a058d72b7db3ee
Meet Kamal Grewal <142540962+meetkamalg@users.noreply.github.com>
2025-07-17 07:08:07 -0600

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,1 +1,22 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
+import CartItem from '../components/CartItem';
+import { Link } from 'react-router-dom';
 
+export default function Cart() {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      {cart.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <>
+          {cart.map(item => <CartItem key={item.id} item={item} />)}
+          <Link to="/checkout"><button>Checkout</button></Link>
+        </>
+      )}
+    </div>
+  );
+}
